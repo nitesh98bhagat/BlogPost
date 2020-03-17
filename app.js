e66cf84ba47f8389fa04ba5b63d681a393484105
Nitesh Bhagat <nitshbhgt1998@gmail.com>
2020-03-17 09:46:06 +0530

    npm set commit & startup boilerPlatecode

---
+++ app.js
@@ -0,0 +1,25 @@
+//imports 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//instance of express server
+const app = express();
+
+//to use static assets such as css, js, & img
+app.use(express.static("public"));
+//to pass data between server and webpage
+app.use(bodyParser.urlencoded({ extended: true }));
+//setting the view engine
+app.set("view engine", "ejs");
+
+//SERVER STARTUP CODE
+app.listen(3000, function () {
+    console.log("SERVER STARTED ON PORT localhost:3000");
+});
+
+
+
+//Home Route
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World</h1>");
+});
