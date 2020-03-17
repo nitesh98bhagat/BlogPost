067528940e65ed27efde7066fcde009e6fd2073d
Nitesh Bhagat <nitshbhgt1998@gmail.com>
2020-03-17 14:49:28 +0530

    created navbar, different route & used w3css lib.

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
+const tweets = ["Hello World, thats my first blog post in my own god damm APP", "oh no!!", "hello"];
+
+
 //instance of express server
 const app = express();
 
@@ -12,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //setting the view engine
 app.set("view engine", "ejs");
 
+
 //SERVER STARTUP CODE
 app.listen(3000, function () {
     console.log("SERVER STARTED ON PORT localhost:3000");
@@ -19,7 +24,26 @@ app.listen(3000, function () {
 
 
 
+
+
 //Home Route
 app.get("/", function (req, res) {
-    res.render("mainBody", { name: "nitesh" });
+    res.render("mainBody", { homeData: tweets });
+});
+
+
+//ADD POST ROUTE
+app.get("/addPosts", function (req, res) {
+    res.render("addPosts");
+});
+
+//ABOUT US
+app.get("/aboutUs", function(req, res){
+    res.render("aboutUs");
+});
+
+
+//ABOUT US
+app.get("/contactUs", function(req, res){
+    res.render("contactUs");
 });
